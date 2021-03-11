151352d8b5f5b4790b54a0666430a9f4ad7ed200
annastasyaivory <annastasya2601@gmail.com>
2021-03-12 01:32:38 +0700

    progres 2

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,78 @@ class _MyAppState extends State<MyApp> {
   // String _tiki = "";
   // String _jne = "";
 
+  var listItem = ["Pos", "Tiki", "JNE"];
+  String _newValue = "Pos";
+  String _result = "";
+
+  List<String> listViewItem = List<String>();
+
+  void perhitunganOngkir() {
+    setState(() {
+      _inputUserAsal = (inputController.text);
+      _inputUserTujuan = (inputController2.text);
+
+      if (_inputUserAsal == "Malang") {
+        if (_inputUserTujuan == "Korea") {
+          if (_newValue == "Pos") {
+            _result = "Rp 303.418,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 374.946,00";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 418.209,00";
+          }
+        } else if (_inputUserTujuan == "Singapore") {
+          if (_newValue == "Pos") {
+            _result = "Rp 268.260,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 144.210,00";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 173.052,00";
+          }
+        } else if (_inputUserTujuan == "Malaysia") {
+          if (_newValue == "Pos") {
+            _result = "Rp 279.218,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 324.472,50";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 360.525,00";
+          }
+        }
+      }
+
+      if (_inputUserAsal == "Jakarta") {
+        if (_inputUserTujuan == "Korea") {
+          if (_newValue == "Pos") {
+            _result = "Rp 300.464,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 374.946,00";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 418.209,00";
+          }
+        } else if (_inputUserTujuan == "Singapore") {
+          if (_newValue == "Pos") {
+            _result = "Rp 268.260,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 144.210,00";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 173.052,00";
+          }
+        } else if (_inputUserTujuan == "Malaysia") {
+          if (_newValue == "Pos") {
+            _result = "Rp 279.218,00";
+          } else if (_newValue == "Tiki") {
+            _result = "Rp 324.472,50";
+          } else if (_newValue == "JNE") {
+            _result = "Rp 360.525,00";
+          }
+        }
+      }
+
+      listViewItem
+          .add("$_inputUserAsal - $_inputUserTujuan [$_newValue] : $_result");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,5 +134,25 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     );
+  }
+
+  DropdownButton<String> buildDropdownButton() {
+    return DropdownButton<String>(
+      items: listItem.map((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+      value: _newValue,
+      onChanged: dropdownOnChanged,
+    );
+  }
+
+  void dropdownOnChanged(String changeValue) {
+    setState(() {
+      _newValue = changeValue;
+      perhitunganOngkir();
+    });
   }
 }
